b05778ae54dcc0c42f6599e09ec7048e244aa0f4
stepsiew <78906315+stepsiew@users.noreply.github.com>
2022-11-17 16:57:32 +0800

    Add files via upload

---
+++ timetable.py
@@ -0,0 +1,56 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1400, 800)
+        self.gridLayoutWidget = QtWidgets.QWidget(Form)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(240, 140, 761, 441))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+       
+        # self.label_name = QtWidgets.QLabel(self.gridLayoutWidget)
+        # self.label_name.setObjectName("name")
+        # self.gridLayout.addWidget(self.label_name, row_num, col_num, occupy_how_many_rows, occupy_how_many_columns)
+
+        self.label_13 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_13.setObjectName("label_13")
+        self.gridLayout.addWidget(self.label_13, 3, 2, 1, 1)
+
+        self.label_17 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_17.setObjectName("label_17")
+        self.gridLayout.addWidget(self.label_17, 4, 2, 3, 1)
+        self.label_15 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_15.setObjectName("label_15")
+        self.gridLayout.addWidget(self.label_15, 5, 0, 1, 1)
+        self.label_14 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_14.setObjectName("label_14")
+        self.gridLayout.addWidget(self.label_14, 1, 2, 2, 1)
+        self.label_11 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_11.setObjectName("label_11")
+        self.gridLayout.addWidget(self.label_11, 0, 2, 1, 1)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 0, 0, 1, 1)
+        self.gridLayout.setColumnStretch(0, 1)
+        self.gridLayout.setColumnStretch(1, 2)
+        self.gridLayout.setColumnStretch(2, 2)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Student Portal"))
+        self.label_13.setText(_translate("Form", "TextLabel"))
+        self.label_17.setText(_translate("Form", "TextLabel"))
+        self.label_15.setText(_translate("Form", "TextLabel"))
+        self.label_14.setText(_translate("Form", "TextLabel"))
+        self.label_11.setText(_translate("Form", "Tuesday"))
+        self.label.setText(_translate("Form", "Monday"))
+        self.label_3.setText(_translate("Form", "Time"))
