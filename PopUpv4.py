e96b8e49096001293d1cff1559b54c643212f3db
Hugolam0806 <93025367+Hugolam0806@users.noreply.github.com>
2022-11-20 06:34:27 +0800

    Update PopUpv4.py

--- PopUpv4.py
+++ PopUpv4.py
@@ -15,29 +15,49 @@ db_connection = mysql.connector.connect(
     database="facerecognition"
 )
 
-def set_student_info(student):
+student_name = ''
+current_student_id = -1
+useremail = ''
+course_id = ''
+class_id = ''
+room = ''
+date = ''
+zoom_link = ''
+start_time = ''
+end_time = ''
+note_link_list = []
+course_teacher = [[]]
+news_list = []
+class_teacher_tuple = ()
+class_type = ''
+remind_msg = ''
+ClassesInformation = ()
+start_to_end_time = ''
+
+def set_info(student):
     global student_name, current_student_id, useremail
+    global course_id, class_id, room, date, zoom_link, start_time, end_time, note_link_list, course_teacher, class_teacher_tuple, class_type
+    global news_list
+    global ClassesInformation
     student_name = student
-    student_name = "JEFF" ### TEMPORARY, TO BE REMOVED!
     info = getStudentInfo(student_name)[0]
     current_student_id = info[0]
     useremail = info[1]
-    global ClassesInformation
-    ClassesInformation = checkclass(current_student_id)
-    global course_id, class_id, room, date, zoom_link, start_time, end_time, note_link_list, class_teacher_tuple, class_type
-    global news_list
-    if ClassesInformation != None:
-        course_id = ClassesInformation[0]
-        class_id = ClassesInformation[1]
-        date = ClassesInformation[2]
-        start_time = ClassesInformation[3]
-        end_time = ClassesInformation[4]
-        room = ClassesInformation[5]
-        zoom_link = ClassesInformation [6]
-        news_list = ClassesInformation[7]
-        note_link_list = ClassesInformation[8]
-        class_teacher_tuple = ClassesInformation[10]
-        class_type = ClassesInformation[11]
+    if checkclass(current_student_id) == None:
+        return
+    ClassesInformation = checkclass(current_student_id)[0]
+    course_id = ClassesInformation[0]
+    class_id = ClassesInformation[1]
+    date = ClassesInformation[2].strftime('%Y-%m-%d')
+    start_time = str(ClassesInformation[3])
+    end_time = str(ClassesInformation[4])
+    room = ClassesInformation[5]
+    zoom_link = ClassesInformation[6]
+    news_list = ClassesInformation[7][0]
+    note_link_list = ClassesInformation[8][0]
+    course_teacher = ClassesInformation[9]
+    class_teacher_tuple = ClassesInformation[10]
+    class_type = ClassesInformation[11]
 
 
 
@@ -73,12 +93,13 @@ def get_note_link():
     global note_link
     note_link = ''
     for i in range(len(note_link_list)):
-        note_link = note_link + " " + "<a href= "+note_link_list[i]+">"+ "Note" + str(i) +"</a>"
+        note_link = note_link + " " + "<a href= "+note_link_list[i]+">"+ "Note" + str(i+1) +"</a>"
 
 class PopUp (QWidget, Ui_PopUp):
     def __init__(self, student):
-        set_student_info(student)
-        test_set_class_info() #hardcoded for testing
+        set_info(student)
+        global ClassesInformation
+        #test_set_class_info() #hardcoded for testing
         super(PopUp, self).__init__()
         self.setupUi(self)
         self.set_course_id()
@@ -97,10 +118,20 @@ class PopUp (QWidget, Ui_PopUp):
         self.email_and_exit_button.clicked.connect(lambda: self.email_and_exit_button_clicked())
 
 
-    def email_and_exit_button_clicked(self,evnt):
-        super(PopUp, self).closeEvent(evnt)
-        #send email here
-        evnt.accept()
+    def email_and_exit_button_clicked(self):
+        # send email here
+        temp = ('id', 'name', 'email', 'office')
+        temp2 = class_teacher_tuple
+        temp3 = ()
+        class_teacher_resultdict = {temp[i]: temp2[i] for i, _ in enumerate(temp2)}
+        course_teacher_dict = {}
+        course_teacher_result = []  # list of dict?
+        for i in range(len(course_teacher)):
+            temp3 = tuple(course_teacher[i])
+            course_teacher_dict = {temp[i]: temp3[i] for i, _ in enumerate(temp3)}
+            course_teacher_result.append(course_teacher_dict)
+
+        send_template(student_name, course_id, class_id, date, start_time, end_time, room, zoom_link, news_list, note_link_list, course_teacher_result, class_teacher_resultdict, class_type)
 
     def set_remind_msg(self):
         get_remind_msg()
@@ -137,13 +168,16 @@ class PopUp (QWidget, Ui_PopUp):
             self.note_link.setText(note_link)
 
     def set_class_teacher(self):
-        self.class_teacher.setText(class_teacher_tuple[1] + "      Email: " + class_teacher_tuple[2] + "      Office: " + class_teacher_tuple[3])
+        if class_teacher_tuple == None:
+            self.class_teacher.setText("N/A")
+        else:
+            self.class_teacher.setText(class_teacher_tuple[1] + "      Email: " + class_teacher_tuple[2] + "      Office: " + class_teacher_tuple[3])
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    myWin = PopUp("JEFF")
-
-    myWin.show()
-
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        myWin = PopUp("JEFF")
+        myWin.show()
+        sys.exit(app.exec_())
+    except:
+        pass
